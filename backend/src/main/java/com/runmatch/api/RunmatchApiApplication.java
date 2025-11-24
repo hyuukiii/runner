80644b32795f59e16aad943c4785077a93edad3e
LG-11GEN\USER <sd>
2025-11-24 11:21:26 +0900

    feat : 백엔드 개발 세팅 추가

---
+++ backend/src/main/java/com/runmatch/api/RunmatchApiApplication.java
@@ -0,0 +1,13 @@
+package com.runmatch.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunmatchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunmatchApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/runmatch/api/RunmatchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runmatch.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunmatchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
