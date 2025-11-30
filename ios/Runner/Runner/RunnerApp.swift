960973c68daa2b0d2fa584de18036ac51750262d
Hyun ki Yun <dbsgusrl1234567@naver.com>
2025-11-30 10:40:19 +0900

    fix: 올바른 git 구조 및 iOS 프로젝트 파일 추가

---
+++ ios/Runner/Runner/RunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunnerApp.swift
+//  Runner
+//
+//  Created by 윤현기 on 11/29/25.
+//
+
+import SwiftUI
+
+@main
+struct RunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
